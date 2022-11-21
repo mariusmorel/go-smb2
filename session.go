c5665bebfd2cbd896e36d7d2aa4c6d767b62fe0d
Mickael-Morel <33719633+Mickael-Morel@users.noreply.github.com>
2022-11-21 15:55:55 +0100

    Update session.go

--- session.go
+++ session.go
@@ -139,8 +139,8 @@ func sessionSetup(conn *conn, i Initiator, ctx context.Context) (*session, error
 		switch conn.dialect {
 		case SMB202, SMB210:
 			log.Println(string(sessionKey))
-			s.signer = hmac.New(sha256.New, sessionKey)
-			s.verifier = hmac.New(sha256.New, sessionKey)
+			//s.signer = hmac.New(sha256.New, sessionKey)
+			//s.verifier = hmac.New(sha256.New, sessionKey)
 		case SMB300, SMB302:
 			signingKey := kdf(sessionKey, []byte("SMB2AESCMAC\x00"), []byte("SmbSign\x00"))
 			ciph, err := aes.NewCipher(signingKey)
